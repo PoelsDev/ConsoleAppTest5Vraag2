c7e93fac83d02c3fd83c8488ec736f217eeb6b19
ThibaultP-immalle <31651240+ThibaultP-immalle@users.noreply.github.com>
2018-03-27 13:08:47 +0200

    Program

--- ConsoleAppTest5Vraag2/Program.cs
+++ ConsoleAppTest5Vraag2/Program.cs
@@ -9,6 +9,21 @@ namespace ConsoleAppTest5Vraag2
     {
         static void Main(string[] args)
         {
+            int a = 10;
+            int b = 1;
+            Console.WriteLine("a = {0} en b = {1}.", a,b);
+
+            SomEnVerschil(ref a, ref b);
+            Console.WriteLine("De som van a en b is {0}, het verschil tussen a en b is {1}.", a, b);
+            
+        }
+
+        static void SomEnVerschil(ref int a, ref int b)
+        {
+            int aCopy;
+            aCopy = a;
+            a = a + b;
+            b = aCopy - b;
         }
     }
 }
